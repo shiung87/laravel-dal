de78aacd2fe7dc0194c18e1a1a55751ebf47e246
Nicholas Ong <shiung87@gmail.com>
2026-05-07 16:05:55 +0800

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
                             <a href="https://cloud.laravel.com" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                                Deploy now
+                                Deploy now 1
                             </a>
                         </li>
                     </ul>
@@ -215,7 +215,7 @@
                 </div>
             </main>
         </div>
-
+       
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
